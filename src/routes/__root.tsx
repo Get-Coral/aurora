c5d367f44585f1c48630b3873d53185239f1a87c
Elian Van Cutsem <hello@elian.codes>
2026-04-06 13:37:14 +0200

    fix: hide header on setup

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,6 +3,7 @@ import {
   Scripts,
   createRootRouteWithContext,
   useRouter,
+  useRouterState,
 } from '@tanstack/react-router'
 import { AppBootstrap } from '../components/AppBootstrap'
 import Footer from '../components/Footer'
@@ -82,6 +83,9 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
 })
 
 function RootDocument({ children }: { children: React.ReactNode }) {
+  const pathname = useRouterState({ select: (state) => state.location.pathname })
+  const hideHeader = pathname === '/setup'
+
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
@@ -93,7 +97,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <I18nProvider>
           <AppBootstrap />
           <PwaStatusBanner />
-          <Header />
+          {hideHeader ? null : <Header />}
           {children}
           <Footer />
           <Scripts />
